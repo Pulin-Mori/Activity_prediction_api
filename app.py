f189c7ffdebddce345f453ffcc717351a5072edb
Pulin-Mori <pulin.mori24123@gmail.com>
2024-03-11 08:06:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ trained_model = pickle.load(open("model.pkl","rb"))
 @app.route('/')
 def home():
     # return jsonify({"Hello World":"1"})
-    ax = request.form.get("Acc_x")
-    ay = request.form.get("Acc_y")
-    az = request.form.get("Acc_z")
-    gx = request.form.get("Gyr_x")
-    gy = request.form.get("Gyr_y")
-    gz = request.form.get("Gyr_z")
+    ax = request.args.get("Acc_x")
+    ay = request.args.get("Acc_y")
+    az = request.args.get("Acc_z")
+    gx = request.args.get("Gyr_x")
+    gy = request.args.get("Gyr_y")
+    gz = request.args.get("Gyr_z")
 
     input = np.array([[gx,gy,gz,ax,ay,az]])
     result = trained_model.predict(input).tolist()[0]
