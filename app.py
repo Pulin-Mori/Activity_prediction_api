f682e83667970a24683fef7f76c2fb238f5641ee
Pulin-Mori <pulin.mori24123@gmail.com>
2024-03-11 16:52:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,19 +21,5 @@ def home():
 
     return jsonify({"Activity":str(result),"ax":str(ax)})
 
-@app.route('/predict', methods = ['GET','POST'])
-def predict():
-    gx = request.form.get('Gyr_x')
-    gy = request.form.get('Gyr_y')
-    gz = request.form.get('Gyr_z')
-    ax = request.form.get('Acc_x')
-    ay = request.form.get('Acc_y')
-    az = request.form.get('Acc_z')
-
-    input = np.array([[gx,gy,gz,ax,ay,az]])
-    result = trained_model.predict(input).tolist()[0]
-
-    return jsonify({"Activity":str(result)})
-
 if __name__=='__main__':
     app.run(debug=True)
