58f164f62fe236e4c39236b7bafdd6559d312acb
Pulin-Mori <pulin.mori24123@gmail.com>
2024-03-16 23:45:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ def home():
     gx = request.args.get("Gyr_x")
     gy = request.args.get("Gyr_y")
     gz = request.args.get("Gyr_z")
-    #grx = request.args.get("Gra_x")
-    #gry = request.args.get("Gra_y")
-    #grz = request.args.get("Gra_z")
+    grx = request.args.get("Gra_x")
+    gry = request.args.get("Gra_y")
+    grz = request.args.get("Gra_z")
 
-    #input = np.array([[ax,ay,az,grx,gry,grz,gx,gy,gz]])
-    input = np.array([[ax,ay,az,gz,gy,gx]])
+    input = np.array([[ax,ay,az,grx,gry,grz,gx,gy,gz]])
+    #input = np.array([[ax,ay,az,gz,gy,gx]])
     result = trained_model.predict(input).tolist()[0]
     
     temp={0:'LAYING',1:'SITTING',2:'STANDING',3:'WALKING',4:'WALKING_DOWNSTAIRS',5:'WALKING_UPSTAIRS'}
